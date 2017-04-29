bff9c381b5c63f2e07990c44229dfa03367e3248
Phlongs <phlongs@gmail.com>
2017-04-28 21:32:44 -0400

    heroku heroku

--- server.js
+++ server.js
@@ -23,8 +23,8 @@ app.use(bodyParser.json({ type: "application/vnd.api+json" }));
 // The below points our server to a series of "route" files.
 // These routes give our server a "map" of how to respond when users visit or request data from various URLs.
 // ================================================================================
-require("./routing/apiRoutes")(app);
-require("./routing/htmlRoutes")(app);
+require("./app/routing/apiRoutes")(app);
+require("./app/routing/htmlRoutes")(app);
 // ==============================================================================
 // LISTENER
 // The below code effectively "starts" our server
